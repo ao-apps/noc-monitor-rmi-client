be462e6418a453cd6426073da1a843f4f07d335c
AO Industries, Inc <support@aoindustries.com>
2021-06-29 02:50:07 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,32 @@
+/*
+ * noc-monitor-rmi-client - RMI Client for Network Operations Center Monitoring.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of noc-monitor-rmi-client.
+ *
+ * noc-monitor-rmi-client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * noc-monitor-rmi-client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with noc-monitor-rmi-client.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.aoindustries.noc.monitor.rmi.client {
+	exports com.aoindustries.noc.monitor.client;
+	// Direct
+	requires com.aoindustries.aoserv.client; // <groupId>com.aoindustries</groupId><artifactId>aoserv-client</artifactId>
+	requires com.aoindustries.noc.monitor.api; // <groupId>com.aoindustries</groupId><artifactId>noc-monitor-api</artifactId>
+	// Java SE
+	requires java.desktop;
+	requires java.rmi;
+	requires java.sql;
+}
